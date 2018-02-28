94d6a0738cec5361c6b1ffab71b9e8cb85d58ab9
miladmemarian <milad.memarian@gmail.com>
2018-02-28 09:42:26 -0800

    I fixed some html, css issues

--- start/main.js
+++ start/main.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-unused-vars */
 function startTimer() {
-  setInterval(function() {
-    seconds++;
-    var $seconds = document.querySelector(".seconds");
-    $seconds.textContent = timer(seconds);
-  }, 1000);
+  setInterval(function () {
+    seconds++
+    var $seconds = document.querySelector('.seconds')
+    $seconds.textContent = timer(seconds)
+  }, 1000)
 }
 
 function timer(seconds) {
-  var minutes = Math.floor(seconds / 60);
-  var seconds = seconds % 60;
-  var timer = minutes + ":" + seconds;
-  return timer;
+  var minutes = Math.floor(seconds / 60)
+  var secs = seconds % 60
+  var timer = minutes + ':' + secs
+  return timer
 }
 
-var seconds = 0;
-var $start = document.querySelector(".start");
+var seconds = 0
+var $start = document.querySelector('.start')
 
-$start.addEventListener("click", startTimer);
+$start.addEventListener('click', startTimer)
