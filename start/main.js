95d921b11927d640d41c0df9dea3fc56b239a1e7
miladmemarian <milad.memarian@gmail.com>
2018-02-28 16:22:27 -0800

    Reset Phase in progress

--- start/main.js
+++ start/main.js
@@ -5,6 +5,9 @@ function startTimer() {
     var $seconds = document.querySelector('.seconds')
     $seconds.textContent = timer(seconds)
   }, 1000)
+  if (hasLimit && seconds === limit) {
+    clearInterval(intervalId)
+  }
 }
 
 function pauseTimer() {
@@ -18,6 +21,12 @@ function resetTimer() {
   seconds = 0
 }
 
+function limitTimer() {
+  $limit = document.querySelector('.input')
+  limit = parseInt($limit.textContent, 10)
+  hasLimit = true
+}
+
 function timer(seconds) {
   var minutes = Math.floor(seconds / 60)
   var secs = seconds % 60
@@ -27,10 +36,15 @@ function timer(seconds) {
 
 var intervalId = 0
 var seconds = 0
+var limit = 0
+var hasLimit = false
 var $start = document.querySelector('.start')
 var $pause = document.querySelector('.pause')
 var $reset = document.querySelector('.reset')
+var $limit = document.querySelector('.limit')
+var $input = document.querySelector('.input')
 
 $start.addEventListener('click', startTimer)
 $pause.addEventListener('click', pauseTimer)
 $reset.addEventListener('click', resetTimer)
+$limit.addEventListener('click', limitTimer)
